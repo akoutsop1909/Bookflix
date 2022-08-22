04189a87dfbce6bef9d171c2342498f8a1fce2bc
Angel Kou <51194516+akoutsop1909@users.noreply.github.com>
2022-08-22 14:19:25 +0300

    Delete php directory

--- php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();
-
-//we just destroy the session. No checks are required.
-session_destroy();
-require_once("utils.php");
-respond();
-?>
-
--- php/me.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-require_once("utils.php");
-
-//ensure user is authenticated
-ensureAuthenticated();
-
-//return active user
-respondObj(getActiveUser());
-?>
